20daf238d2e7b5153b4be1a6acab091e1737aa84
Manash <manashmndl@gmail.com>
2020-05-02 02:20:13 +0600

    finally fix the bar chart

--- void.js
+++ void.js
@@ -1,4 +1,5 @@
-<Row>
+{
+  /* <Row>
   <Col xs={10} sm={8} md={6} lg={4} xl={2}></Col>
   <Col>
     <span>
@@ -8,4 +9,25 @@
     </span>
   </Col>
   <Col xs={10} sm={8} md={6} lg={4} xl={2}></Col>
-</Row>;
+</Row>; */
+}
+
+let data = JSON.parse(
+  '{"allPost":{"nodes":[{"__typename":"MdxPost","tags":[{"name":"C++","slug":"c"}]},{"__typename":"MdxPost","tags":[{"name":"python","slug":"python"},{"name":"C++","slug":"c"},{"name":"aws","slug":"aws"}]},{"__typename":"MdxPost","tags":[{"name":"python","slug":"python"},{"name":"C++","slug":"c"},{"name":"aws","slug":"aws"}]}]}}'
+);
+
+const _ = require("lodash");
+let tags = data.allPost.nodes
+  .map((node) => node.tags.map((t) => t.name))
+  .flat(2);
+let groupTags = _.groupBy(tags);
+let countByTag = Object.keys(groupTags).map((gt) => ({
+  tag: gt,
+  count: groupTags[gt].length,
+}));
+
+console.log(countByTag);
+
+console.log(groupTags);
+
+// console.log(_.groupBy(tags, "length"));
