9b00530727af1cc385a33b6c90d167f472306470
Manash <manashmndl@gmail.com>
2020-05-06 08:06:49 +0600

    adding components

---
+++ content/posts/components/CppListPosts.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { List, Avatar } from "antd";
+import "antd/dist/antd.css";
+
+const data = [
+  {
+    title: "Ant Design Title 1",
+  },
+  {
+    title: "Ant Design Title 2",
+  },
+];
+
+const CppListPosts = () => <h1></h1>;
+
+export default CppListPosts;
---
+++ content/posts/components/ListItems.js
@@ -0,0 +1,3 @@
+import React from "react";
+import { List, Avatar } from "antd";
+import "antd/dist/antd.css";
